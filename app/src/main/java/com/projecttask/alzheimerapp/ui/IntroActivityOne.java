ef33a69d5f4635d19bfc594545e37aaa440cb24a
hahrahr <hammedsaudix@gmail.com>
2023-12-06 22:50:51 +0200

    Creating App with SplashActivity and introActivity

---
+++ app/src/main/java/com/projecttask/alzheimerapp/ui/IntroActivityOne.java
@@ -0,0 +1,28 @@
+package com.projecttask.alzheimerapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.projecttask.alzheimerapp.R;
+import com.projecttask.alzheimerapp.ui.auth.RegisterActivity;
+
+public class IntroActivityOne extends AppCompatActivity {
+    protected Button intButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_one);
+
+        intButton = findViewById(R.id.introBtn);
+
+        intButton.setOnClickListener(View ->{
+            Intent intent = new Intent(IntroActivityOne.this, RegisterActivity.class);
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/projecttask/alzheimerapp/ui/auth/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.projecttask.alzheimerapp.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projecttask.alzheimerapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
